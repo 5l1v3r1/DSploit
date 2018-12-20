076781bd000d477ef25d31699bc9cb45f3aea4fd
TSMaitry <36931727+TSMaitry@users.noreply.github.com>
2018-12-20 13:41:33 +0530

    Add files via upload

---
+++ banner/banner4.py
@@ -0,0 +1,10 @@
+# Set Color
+print (      "\033[0;37;41m Exploit Scanner  v0.2 \033")
+print      " _____      _____       "
+print "|  __ \    / ____|     "
+print "| |  | |  | (___       "
+print "| |  | |   \___ \      "
+print "| |__| |   ____) |     "
+print "|_____/ IM|_____/ PLOIT"
+                 
+                 
